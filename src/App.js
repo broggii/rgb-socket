3167d0ff37c6211aa4ba0949bd600f47b585fa17
Cameron Walworth <cameron.walworth@gmail.com>
2018-09-01 14:54:42 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <button>R</button>
+        <button>G</button>
+        <button>B</button>
+      </div>
+    );
+  }
+}
+
+export default App;
